8b2952985d13703581c5a09317a48dda428b9bc4
dimisyko <dimitri.syko@hotmail.com>
2023-01-10 16:28:10 +0100

    in progress

--- src/assets/js/libs/splitTxt.js
+++ src/assets/js/libs/splitTxt.js
@@ -1,15 +1,9 @@
-export default class splitTxt {
-    constructor({ el, spanClassFirst, spanClassSecond }) {
-        this.el = el
-        this.spanClassFirst = spanClassFirst
-        this.spanClassSecond = spanClassSecond
-        this.cutWords()
-    }
-    cutWords(){
-        const cut = this.el.textContent.split(' ')
-        this.el.textContent = ""
+export default function splitTxt(el, spanClassFirst, spanClassSecond) {
+    (function cutWords() {
+        const cut = el.textContent.split(' ')
+        el.textContent = ""
         for (let index = 0; index < cut.length; index++) {
-            this.el.innerHTML+= '<span class="'+this.spanClassFirst+' hide"><span class="'+this.spanClassSecond+'">'+cut[index]+'</span></span>'
+            el.innerHTML += '<span class="' + spanClassFirst + ' hide"><span class="' + spanClassSecond + '">' + cut[index] + '</span></span>'
         }
-    }
+    })()
 }
